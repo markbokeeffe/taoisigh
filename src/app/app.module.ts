b78a18ca4265002d04c73f0169f6aeb440e20315
markokeeffe <markokeeffe@vsware.ie>
2017-11-13 16:59:23 +0000

    Initial base

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HomeComponentComponent } from './home-component/home-component.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponentComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule
